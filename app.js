d9c72fe6e288f5c660380e4fb786563f2c336228
Makineni Bala Krishna <95757885+BalaKrishna002@users.noreply.github.com>
2025-02-22 15:15:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 // Enable CORS for all origins (or restrict to frontend domain if needed)
 // app.use(cors({
